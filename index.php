f0c89e4cc7cd47a8923ea9504ab8445530c35b7a
August Barrus <67875711+abarrus@users.noreply.github.com>
2026-02-21 15:24:39 -0500

    messing around

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+echo 'My first PHP script!';
+?>
+<br>
+hi
+<button>make game</button>
+<div style="border:1px solid black">
+    <h3>all the games</h3>
+</div>
+</body>
+</html>
