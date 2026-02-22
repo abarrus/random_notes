fa102af38e292a5a7771767c192e3a9d6c9fc33f
August Barrus <67875711+abarrus@users.noreply.github.com>
2026-02-22 15:43:07 -0500

    No game yet but we have a start screen where you can click make game or join game

---
+++ create_game.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'db.php';
+
+$nickname = $_POST["nickname"];
+$gameName = $_POST["game-name"];
+
+// no empty vars
+if (strlen($gameName) === 0) {
+    $gameName = "Game";
+}
+
+echo("name".$gameName);
+
+$sql = "INSERT INTO Games (last_changed, status, name)
+VALUES (NOW(), 'open', ?)";
+$stmt = $conn -> prepare($sql);
+$stmt->execute([$gameName]);
--- index.php
+++ index.php
@@ -1,15 +1,70 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- my styles -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
 <body>
- 
-<?php
-echo 'My first PHP script!';
-?>
-<br>
-hi
-<button>make game</button>
-<div style="border:1px solid black">
-    <h3>all the games</h3>
-</div>
+    <div class="main-content">
+        <form method="POST" action="create_game.php">
+            <div class="form-group">
+                <label for="nickname">Your Nickname:</label>
+                <input type="text" class="form-control" name="nickname" aria-describedby="yourNickname" placeholder="Name here...">
+            </div>
+            <div class="card container-fluid overflow-hidden text-center">
+                <div class="row">
+                    <!-- Make Game half -->
+                    <div class="col bg-light py-4">
+                        <h2>Make Game</h2>
+                        <hr>
+                        <input type="text" class="form-control mb-2" name="game-name" aria-describedby="gameName" placeholder="Name here...">
+                        <button type="submit" class="btn btn-primary">Make Game</button>
+                    </div>
+                    <!-- Join Game half -->
+                    <div class="col bg-dark text-light py-4">
+                        <h2>Join Game</h2>
+                        <hr>
+                        <div id="games">
+                        </div>
+                    </div>
+                </div>
+                <!-- end card -->
+            </div>
+        </form>
+    </div>
+    <script>
+        function loadGames() {
+            function addGameToList(id, name, container) {
+                container.innerHTML += `
+                <div class="row">
+                    <button class="btn btn-dark rounded-0" type="submit">${name}</button>
+                </div>`;
+            }
+            
+            fetch("list_games.php")
+                .then(res => res.json())
+                .then(data => {
+                    const games = data;
+
+                    const container = document.getElementById("games");
+                    container.innerHTML = ''; // clear old stuff
+
+                    games.forEach(game => {
+                        if (game.status == "open") {
+                            if (game.name === "") {
+                                game.name = "Untitled";
+                            }
+                            addGameToList(game.id, game.name, container);
+                        }
+                    });
+                });
+        }
+        loadGames();
+    </script>
 </body>
+
 </html>
