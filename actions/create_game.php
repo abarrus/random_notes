5ed20c6b45b5a3f26b9894c27670c57eb29b01ce
August Barrus <67875711+abarrus@users.noreply.github.com>
2026-03-13 21:51:57 -0400

    delete inaccurate comment

--- actions/create_game.php
+++ actions/create_game.php
@@ -15,8 +15,6 @@ $gameId = bin2hex(random_bytes(8)); // 16-char hex
 
 make_game($gameName, $gameId);
 
-// TODO - go to game instead. can't have 2 headers.
-// this part of the code doesn't run yet btw
 join_game($gameId, $nickname);
 header("Location: /submission_write.php");
 exit;
