416b8d38232fd0f9b74c0f8e1ebbb547e8518a66
August Barrus <67875711+abarrus@users.noreply.github.com>
2026-03-16 17:17:10 -0400

    hopefully working print_db, and testing4

--- helpers/print_db.php
+++ helpers/print_db.php
@@ -26,14 +26,16 @@ $conn = connect();
 // make and truncate tables
 foreach ($tables as $name) {
     try {
-        $stmt = $conn->prepare("SELECT * FROM ?");
-        $stmt->execute([$name]);
+        $stmt = $conn->prepare("SELECT * FROM " . $name . " LIMIT 50");
+        $stmt->execute();
 
         $res = $stmt->fetchAll();
-        echo("<h2>".$name."<h2>");
+        echo "<h2>" . $name . "</h2>";
+        echo "<pre>";
         print_r($res);
+        echo "</pre>";
 
-        echo("<br><br>");
+        echo "<br><br>";
     } catch (PDOException $e) {
         echo "Error printing $name: " . $e->getMessage() . "<br>";
     }
