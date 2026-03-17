68afeb5db4ffc77307d4a3d5d43d94c4393f9bf7
August Barrus <67875711+abarrus@users.noreply.github.com>
2026-03-17 09:51:51 -0400

    db_connect is its own PHP file now. also got rid of gitignore I'm giving up figuring out .env file and instead I will just change db_connect when I test locally

--- helpers/db.php
+++ helpers/db.php
@@ -1,21 +1,5 @@
 <?php
-
-function connect()
-{
-  $servername = getenv('servername');
-  $username = getenv('username');
-  $password = getenv('password');
-  $dbname = getenv('dbname');
-
-  try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $conn;
-  } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-  }
-}
+include "db_connect.php";
 
 // Returns, as an array, all words that this player has in this game
 function get_words($gameId, $playerId)
