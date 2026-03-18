ddb3b0ae43676575662838d627f17a7fa7df354f
August Barrus <67875711+abarrus@users.noreply.github.com>
2026-03-18 17:05:00 -0400

    db connect function uses a static variable for $conn so we can keep calling it multiple times without re-connecting to the db

--- helpers/db_connect.php
+++ helpers/db_connect.php
@@ -2,17 +2,26 @@
 
 function connect()
 {
-  $servername = getenv('servername');
-  $username = getenv('username');
-  $password = getenv('password');
-  $dbname = getenv('dbname');
+    static $conn = null; // this stays across function calls
 
-  try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if ($conn === null) {
+        $servername = getenv('servername');
+        $username = getenv('username');
+        $password = getenv('password');
+        $dbname = getenv('dbname');
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            
+            // set the PDO error mode to exception
+            // the other error modes either ignore errors or warn you but keep going
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $conn;
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
     return $conn;
-  } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-  }
 }
